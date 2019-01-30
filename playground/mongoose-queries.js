7261ae283bf5429113af7bc2be7710617aad1d1a
Malachi Ivey <zarekbusiness@gmail.com>
2019-01-30 12:42:25 -0500

    setup app for heroku

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -3,30 +3,30 @@ const {ObjectID} = require('mongoose')
 const {mongoose} = require('./../server/db/mongoose')
 const {Todo}  = require('./../server/models/todo')
 
-let id = '5c51b908e1980e90168dc3bc'
+// let id = '5c51b908e1980e90168dc3bc'
 
-if (!ObjectID.isValid(id)) {
-    console.log('ID not found');
-}
+// if (!ObjectID.isValid(id)) {
+//     console.log('ID not found');
+// }
 
-// finds all
-Todo.find({
-    _id: id
-}).then((todos) => {
-    console.log('Todos', todos);
-});
+// // finds all
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
 
-// find the first one that matches
-Todo.findOne({
-    _id: id
-}).then((todo) => {
-    console.log('Todo', todo);
-});
+// // find the first one that matches
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     console.log('Todo', todo);
+// });
 
-// find by id
-Todo.findById(id).then((todo) => {
-    if (!todo) {
-        return console.log('Id not found')
-    }
-    console.log('Todo by Id', todo);
-}).catch((e) => console.log(e));
+// // find by id
+// Todo.findById(id).then((todo) => {
+//     if (!todo) {
+//         return console.log('Id not found')
+//     }
+//     console.log('Todo by Id', todo);
+// }).catch((e) => console.log(e));

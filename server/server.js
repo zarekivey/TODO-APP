63048972179ffe0d6f21336b026f91f099225917
Malachi Ivey <zarekbusiness@gmail.com>
2019-01-30 09:35:14 -0500

    get todos route

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     let todo = new Todo({
         text: req.body.text
-    })
+    });
 
     todo.save().then((doc) => {
         res.send(doc);
@@ -21,6 +21,13 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
 
 app.listen(3000, () => {
     console.log('server live on port 3000')
